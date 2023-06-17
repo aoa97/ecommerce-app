145b0419f257f3ac8bbbd2460ca17f16a399347e
Ahmed Usama <eng.ahmedusama@yahoo.com>
2023-06-17 15:06:51 +0300

    Init theme & colors

---
+++ lib/core/presentation/utils/colors.dart
@@ -0,0 +1,13 @@
+import 'dart:ui';
+
+class Palette {
+  static const primary = Color(0xFF19A7CE);
+  static const secondary = Color(0xFFFEFF86);
+  static const background = Color(0xFFF9F9F9);
+  static const grey = Color(0xFF9B9B9B);
+  static const black = Color(0xFF222222);
+  static const white = Color(0xFFFFFFFF);
+  static const success = Color(0xFF00DFA2);
+  static const error = Color(0xFFF01F0E);
+  static const hot = Color(0xFFFF6969);
+}
--- lib/core/presentation/widgets/main_app.dart
+++ lib/core/presentation/widgets/main_app.dart
@@ -1,5 +1,7 @@
 import 'package:ecommerce/core/presentation/models/flavor_config.dart';
+import 'package:ecommerce/core/presentation/utils/colors.dart';
 import 'package:ecommerce/core/presentation/utils/enums.dart';
+import 'package:ecommerce/features/home/presentation/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 
 class MainApp extends StatelessWidget {
@@ -12,6 +14,16 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: flavor.type == Flavor.development,
       title: flavor.appTitle,
+      home: const HomeScreen(),
+      theme: ThemeData(
+        useMaterial3: true,
+        materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Palette.primary,
+          background: Palette.background,
+          error: Palette.error,
+        ),
+      ),
     );
   }
 }
