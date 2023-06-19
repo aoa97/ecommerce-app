9ed432a6df84a3686fd34d0c9324912696d20f26
Ahmed Usama <eng.ahmedusama@yahoo.com>
2023-06-19 18:22:58 +0300

    Setup dio & Add ApiService

--- lib/features/home/presentation/screens/home_screen.dart
+++ lib/features/home/presentation/screens/home_screen.dart
@@ -1,7 +1,8 @@
+import 'package:ecommerce/core/application/api_service.dart';
 import 'package:ecommerce/core/features/locale/presentation/controller/locale_cubit.dart';
-import 'package:ecommerce/core/presentation/widgets/loading_indicator.dart';
+import 'package:ecommerce/core/presentation/services/service_locator.dart';
+import 'package:ecommerce/l10n/l10n.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -11,12 +12,17 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          context.read<LocaleCubit>().switchLocale();
+          // context.read<LocaleCubit>().switchLocale();
+          sl<ApiService>().get(endpoint: "/users");
         },
       ),
-      body: const SafeArea(
-        child: Center(
-          child: LoadingIndicator(),
+      body: Scaffold(
+        appBar: AppBar(title: Text(tr(context).welcome)),
+        body: Center(
+          child: OutlinedButton(
+            onPressed: sl<LocaleCubit>().switchLocale,
+            child: const Text("Switch Locale"),
+          ),
         ),
       ),
     );
--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,5 +1,5 @@
 import 'package:ecommerce/core/presentation/models/flavor_config.dart';
-import 'package:ecommerce/core/presentation/services/main_initializer.dart';
+import 'package:ecommerce/core/presentation/services/service_locator.dart';
 import 'package:ecommerce/core/presentation/utils/enums.dart';
 import 'package:ecommerce/core/presentation/widgets/main_app.dart';
 import 'package:flutter/material.dart';
@@ -7,12 +7,17 @@ import 'package:flutter/material.dart';
 final _config = FlavorConfig(
   type: Flavor.production,
   appTitle: "E-Commerce",
-  apiBaseUrl: "https://qc.eommerce.com",
+  apiBaseUrl: "https://eommerce.com",
 );
 
 void main() async {
-  await mainInitializer();
+  await _mainInitializer();
   runApp(
     MainApp(flavor: _config),
   );
+}
+
+Future<void> _mainInitializer() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  ServiceLocator.init(apiBaseUrl: _config.apiBaseUrl);
 }
