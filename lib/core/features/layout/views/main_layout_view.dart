98dd7a61689ccb7e13da473351046143a9b49be8
Ahmed Usama <eng.ahmedusama@yahoo.com>
2023-06-22 00:40:01 +0300

    Update BotomNav icons

--- lib/core/features/layout/views/main_layout_view.dart
+++ lib/core/features/layout/views/main_layout_view.dart
@@ -1,3 +1,5 @@
+import 'package:ecommerce/assets/assets.gen.dart';
+import 'package:ecommerce/core/presentation/utils/colors.dart';
 import 'package:ecommerce/l10n/l10n.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
@@ -12,13 +14,38 @@ class MainLayoutView extends HookWidget {
   Widget build(BuildContext context) {
     final selectedIndex = useState<int>(0);
 
-    final destinations = useMemoized(
+    final items = useMemoized(
       () => [
-        (icon: const Icon(Icons.home), label: tr(context).home, path: "/"),
-        (icon: const Icon(Icons.shop), label: tr(context).shop, path: "/shop"),
-        (icon: const Icon(Icons.card_travel), label: tr(context).cart, path: "/bag"),
-        (icon: const Icon(Icons.favorite), label: tr(context).favorites, path: "/favorites"),
-        (icon: const Icon(Icons.person), label: tr(context).profile, path: "/profile"),
+        (
+          icon: Assets.icons.home.svg(width: 30),
+          activeIcon: Assets.icons.homeFilled.svg(width: 30),
+          label: tr(context).home,
+          path: "/"
+        ),
+        (
+          icon: Assets.icons.shop.svg(width: 30),
+          activeIcon: Assets.icons.shopFilled.svg(width: 30),
+          label: tr(context).shop,
+          path: "/shop",
+        ),
+        (
+          icon: Assets.icons.bag.svg(width: 30),
+          activeIcon: Assets.icons.bagFilled.svg(width: 30),
+          label: tr(context).cart,
+          path: "/bag",
+        ),
+        (
+          icon: Assets.icons.heart.svg(width: 30),
+          activeIcon: Assets.icons.heartFilled.svg(width: 30),
+          label: tr(context).favorites,
+          path: "/favorites",
+        ),
+        (
+          icon: Assets.icons.person.svg(width: 30),
+          activeIcon: Assets.icons.personFilled.svg(width: 30),
+          label: tr(context).profile,
+          path: "/profile",
+        ),
       ],
       [],
     );
@@ -26,11 +53,21 @@ class MainLayoutView extends HookWidget {
     return Scaffold(
       body: child,
       appBar: AppBar(title: const Text("TITLE")),
-      bottomNavigationBar: NavigationBar(
-        selectedIndex: selectedIndex.value,
-        destinations: [...destinations.map((e) => NavigationDestination(label: e.label, icon: e.icon))],
-        onDestinationSelected: (i) {
-          context.go(destinations[i].path);
+      bottomNavigationBar: BottomNavigationBar(
+        showUnselectedLabels: true,
+        selectedItemColor: Palette.primary,
+        unselectedItemColor: Palette.grey,
+        currentIndex: selectedIndex.value,
+        selectedLabelStyle: const TextStyle(fontSize: 12, fontWeight: FontWeight.w500),
+        unselectedLabelStyle: const TextStyle(fontSize: 12, height: 1.5),
+        type: BottomNavigationBarType.fixed,
+        items: [
+          ...items.map(
+            (e) => BottomNavigationBarItem(label: e.label, icon: e.icon, activeIcon: e.activeIcon),
+          )
+        ],
+        onTap: (i) {
+          context.go(items[i].path);
           selectedIndex.value = i;
         },
       ),
