bc749f05609a260d6137fe1d588b5c0cc6375e53
Ahmed Usama <eng.ahmedusama@yahoo.com>
2023-08-10 13:41:54 +0300

    Update HomeProducts

--- lib/features/home/presentation/ui/containers/home_products.dart
+++ lib/features/home/presentation/ui/containers/home_products.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerce/core/data/models/product_item_model.dart';
 import 'package:ecommerce/core/presentation/widgets/loading_indicator.dart';
 import 'package:ecommerce/core/presentation/widgets/main_error_widget.dart';
 import 'package:ecommerce/features/home/presentation/controller/home_cubit.dart';
@@ -17,29 +18,33 @@ class HomeProducts extends StatelessWidget {
         final cubit = context.read<HomeCubit>();
         final products = cubit.products;
 
-        if (state is HomeResourcesFulfilled) {
-          return GridView.builder(
-            padding: EdgeInsets.fromLTRB(20.h, 20.h, 20.h, 10.h),
-            itemCount: products.length,
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              crossAxisSpacing: 15.h,
-              mainAxisSpacing: 15.h,
-              childAspectRatio: .8,
-            ),
-            itemBuilder: (context, index) => ProductGridItem(item: products[index]),
-          );
-        }
-
-        if (state is HomeResourcesRejected) {
-          return MainErrorWidget(
-            error: state.errorModel,
-            onRefresh: cubit.fetchResources,
-          );
-        }
-
-        return const Center(child: LoadingIndicator());
+        return switch (state) {
+          HomeResourcesFulfilled() => _Grid(products: products),
+          HomeResourcesRejected() => MainErrorWidget(error: state.errorModel, onRefresh: cubit.fetchResources),
+          _ => const Center(child: LoadingIndicator()),
+        };
       },
+    );
+  }
+}
+
+class _Grid extends StatelessWidget {
+  const _Grid({required this.products});
+
+  final List<ProductItemModel> products;
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      padding: EdgeInsets.fromLTRB(20.h, 20.h, 20.h, 10.h),
+      itemCount: products.length,
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        crossAxisSpacing: 15.h,
+        mainAxisSpacing: 15.h,
+        childAspectRatio: .8,
+      ),
+      itemBuilder: (context, index) => ProductGridItem(item: products[index]),
     );
   }
 }
