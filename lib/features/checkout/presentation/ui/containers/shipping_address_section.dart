3f4d8480bc29f3b00702516a8b6073e4fab8173c
Ahmed Usama <eng.ahmedusama@yahoo.com>
2023-08-10 15:38:39 +0300

    Provide translations to address module

--- lib/features/checkout/presentation/ui/containers/shipping_address_section.dart
+++ lib/features/checkout/presentation/ui/containers/shipping_address_section.dart
@@ -1,5 +1,6 @@
 import 'package:ecommerce/core/presentation/utils/extensions.dart';
 import 'package:ecommerce/core/presentation/widgets/shipping_address_card.dart';
+import 'package:ecommerce/l10n/l10n.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
@@ -16,7 +17,7 @@ class ShippingAddressSection extends StatelessWidget {
         ShippingAddressCard(
           fullName: "John Doe",
           address: "3 Newbridge Court\nChino Hills, CA 91709, United States",
-          actionLabel: "Change",
+          actionLabel: tr(context).change,
           onAction: () => context.push("/addresses"),
         ),
       ],
